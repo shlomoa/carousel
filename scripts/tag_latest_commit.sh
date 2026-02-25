9026b898586c2d02781a108067fb2d50ae727bb4
shlomoa <shlomo.anglister@gmail.com>
2026-02-25 15:47:59 +0200

    last one

--- scripts/tag_latest_commit.sh
+++ scripts/tag_latest_commit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-new_tag=$(/usr/bin/git tag --merged HEAD |  /usr/bin/tail -1 | /usr/bin/perl -pe 's/(.*)(\d+)\n/$1.($2+1)/e')
+GITNEWTAG=$(/usr/bin/git tag --merged HEAD |  /usr/bin/tail -1 | /usr/bin/perl -pe 's/(.*)(\d+)\n/$1.($2+1)/e')
 
-/usr/bin/git tag -a -m "a new version: $new_tag" $new_tag > /dev/null 2>&1
+/usr/bin/git tag -a -m "a new version: $GITNEWTAG" $GITNEWTAG > /dev/null 2>&1
 
-printf "%s" "$new_tag"
+printf "%s" "$GITNEWTAG"
