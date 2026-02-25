64e73750610d932796d28f481c5ff61745b901a4
shlomoa <shlomo.anglister@gmail.com>
2026-02-25 14:56:34 +0200

    Maybe

--- scripts/tag_latest_commit.sh
+++ scripts/tag_latest_commit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-new_tag=$(/usr/bin/git tag --merged HEAD |  /usr/bin/tail -1 | /usr/bin/perl -pe 's/(.*)(\d+)/$1.($2+1)/e')
+new_tag=$(/usr/bin/git tag --merged HEAD |  /usr/bin/tail -1 | /usr/bin/perl -pe 's/(.*)(\d+)\n/$1.($2+1)/e')
 
 /usr/bin/git tag -a -m "a new version: $new_tag" $new_tag
 
-echo $new_tag
+printf $new_tag
