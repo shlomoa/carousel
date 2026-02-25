3f8b314a3ebb7da1da2263401a9f64b4779673a4
shlomoa <shlomo.anglister@gmail.com>
2026-02-25 16:01:48 +0200

    noo

--- scripts/tag_latest_commit.sh
+++ scripts/tag_latest_commit.sh
@@ -3,5 +3,6 @@
 GITNEWTAG=$(/usr/bin/git tag --merged HEAD |  /usr/bin/tail -1 | /usr/bin/perl -pe 's/(.*)(\d+)\n/$1.($2+1)/e')
 
 /usr/bin/git tag -a -m "a new version: $GITNEWTAG" $GITNEWTAG > /dev/null 2>&1
+/usr/bin/git push origin --tags > /dev/null 2>&1
 
 printf "%s" "$GITNEWTAG"
