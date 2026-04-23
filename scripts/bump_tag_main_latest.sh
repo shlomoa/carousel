a24c141599643f18d03df89aa983a100e1eb49cf
shlomoa <shlomo.anglister@gmail.com>
2026-04-23 20:32:52 +0300

    last

--- scripts/bump_tag_main_latest.sh
+++ scripts/bump_tag_main_latest.sh
@@ -2,7 +2,7 @@
 
 GITNEWTAG=$(git tag --merged HEAD |  tail -1 | perl -pe 's/(.*)(\d+)\n/$1.($2+1)/e')
 
-git tag -a "$GITNEWTAG" -m "a new version: $GITNEWTAG"  > /dev/null 2>&1
-git push origin --tags > /dev/null 2>&1
+git tag -a "$GITNEWTAG" -m "a new version: $GITNEWTAG"
+git push origin $GITNEWTAG
 
 printf "%s" "$GITNEWTAG"
